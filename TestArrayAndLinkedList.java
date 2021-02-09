138d256af636dc50ada0797d89835b540db9a3ab
jimmy-kyalo <kyalo90@gmail.com>
2021-02-09 14:36:01 +0300

    java

---
+++ TestArrayAndLinkedList.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class TestArrayAndLinkedList {
+
+    public static void main(String[] args) {
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(1);
+        arrayList.add(4);
+        arrayList.add(0, 10);
+        arrayList.add(3, 30);
+
+        System.out.println("A list of integers in the array list:");
+        System.out.println(arrayList);
+
+        // linked list
+        LinkedList<Object> linkedList = new LinkedList<Object>(arrayList);
+        linkedList.add(1, "red");
+        linkedList.removeLast();
+        linkedList.addFirst("green");
+
+        // list iterator
+        System.out.println("Display the linked list forward:");
+        ListIterator<Object> listIterator = linkedList.listIterator();
+        while (listIterator.hasNext()) {
+            System.out.print(listIterator.next() + " ");
+        }
+        System.out.println();
+
+        // list iterator
+        System.out.println("Display the linked list backward:");
+        listIterator = linkedList.listIterator(linkedList.size());
+        while (listIterator.hasPrevious()) {
+            System.out.print(listIterator.previous() + " ");
+        }
+    }
+}
