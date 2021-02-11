6b7d6afa9b94028771f1d7bcb0ff0c6a800ce141
jimmy-kyalo <kyalo90@gmail.com>
2021-02-11 14:43:44 +0300

    java

--- SortStringByLength.java
+++ SortStringByLength.java
@@ -4,7 +4,8 @@ public class SortStringByLength {
 
     public static void main(String[] args) {
         String[] cities = { "Atlanta", "Savannah", "New York", "Dallas" };
-        Arrays.sort(cities, new MyComparator());
+        // Arrays.sort(cities, new MyComparator());
+        Arrays.sort(cities, Comparator.comparing(String::length));
         // Arrays.sort(cities,(s1, s2) −> s1.length() − s2.length());
 
         for (String s : cities) {
@@ -12,10 +13,10 @@ public class SortStringByLength {
         }
     }
 
-    public static class MyComparator implements Comparator<String> {
-        @Override
-        public int compare(String s1, String s2) {
-            return s1.length() - s2.length();
-        }
-    }
+    // public static class MyComparator implements Comparator<String> {
+    // @Override
+    // public int compare(String s1, String s2) {
+    // return s1.length() - s2.length();
+    // }
+    // }
 }
